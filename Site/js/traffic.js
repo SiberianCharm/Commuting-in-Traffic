3f791387a458f6eedd7882f6cb69d85cbc683278
Pamela Castillo <pamela@marketpublique.com>
2011-07-17 14:30:41 -0400

    Added Icon function

--- Site/js/traffic.js
+++ Site/js/traffic.js
@@ -51,12 +51,12 @@ HHH.MouseOverMenu = function(){
 	});
 }
 
-/*HHH.Transition = function(){
+HHH.Transition = function(){
 //Transition animation
 	$(".photo").fadeOut();
 	$(".photo").attr("src", "new url");
 	$(".photo").fadeIn();
-} */
+}
 
 
 HHH.PopulatePage = function(questionID){
@@ -68,20 +68,27 @@ HHH.PopulatePage = function(questionID){
 
 HHH.LoadQuestion = function(questionID){
 	var question = jlinq.from(HHH.Data.questions)
-	.equals("ID", questionID.toString())
+	.equals("ID", questionID)
 	.select();
-	$("#question").html('<div class="inside clearfix"><p class="large heading">' + question[0].question_heading + '</p><p>' + question[0].question_content + '</p></div><div class="options"></div>');
+	$("#question").val('<div class="inside clearfix"><p class="large heading">' + question.question_heading + "</p><p>" + question.content + "</p></div>");
+	var location = question.question_location;
+	HHH.setIcon(location);
+	HHH.setProgress(location);
 }
 
+/*HHH.setIcon = function(locationID){
+	var location = jlinq.from(HHH.Data.locations)
+	.equals("ID", locationID)
+	$("#icon").val('<img src="'+ locations.location_icon + "'/>");
+}*/
+
 HHH.LoadOptions = function(questionID){
-	var option = jlinq.from(HHH.Data.options)
-	.equals("option_question", questionID.toString())
+	var options = jlinq.from(HHH.Data.options)
+	.equals("option_question", questionID)
 	.select();
-	$("#options").html("");
-	for(i=0; i < option.length; i++)
+	for(i=0; i < options.length; i++)
 	{
-		console.log($("#question").children(".options"));
-		$("#question").children(".options").append('<div id="opt_'+ option[i].option_next_question + ' class="button" onclick="HHH.OptionClick"><span>' + option[i].option_name + '</span> <br />' + option[i].option_about + '</div>' );
+		$(".options").append('<div id="opt_'+ option.option_next_question + 'class="button" onclick="HHH.OptionClick"><span>' option.name + '</span> <br />' + option.about + '</div>' );
 	}
 }
 
@@ -89,8 +96,11 @@ HHH.LoadFact = function(questionID){
 	var fact = jlinq.from(HHH.Data.facts)
 	.equals("fact_question", questionID)
 	.select();
-	console.log(fact[0].fact_did_you_know_short);
-	$("#fact").children("div").children()[2].value = fact[0].fact_did_you_know_short;
+	$(".inside").children("p").val(fact.anecdote);
+}
+
+HHH.getProgress = function (questionID){
+	var progressIcon = 
 }
 
 //Google Analytics tracking
